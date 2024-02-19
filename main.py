0c2d2e3b2ed476ad0061ce9dae47977d79b777ec
Pedro da Fonseca Schwertner <81368907+AtomicAnt27@users.noreply.github.com>
2024-02-19 20:43:32 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# funcionalidade de enviar mensagem
+@socketio.on("message")
+def gerenciar_mensagens(mensagem):
+    send(mensagem, broadcast=True)
+
+# criar a 1° página = 1° rota
+@app.route("/") # decorator
+def homepage():
+    return render_template("homepage.html")
+
+# roda o nosso aplicativo
+socketio.run(app, host="192.168.0.12")
+
+# websocket
